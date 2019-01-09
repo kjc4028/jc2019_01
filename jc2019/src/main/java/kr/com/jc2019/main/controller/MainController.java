3148f8be982df408ef8bed3eac89ffbfd866f8fb
kjc <whd4028@gmail.com>
2019-01-09 18:09:21 +0900

    setting 20190109

--- jc2019/src/main/java/kr/com/jc2019/main/controller/MainController.java
+++ jc2019/src/main/java/kr/com/jc2019/main/controller/MainController.java
@@ -1,15 +1,15 @@
-package kr.com.jc2019.main;
+package kr.com.jc2019.main.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.com.jc2019.main.service.Test01Service;
 
 /**
  * Handles requests for the application home page.
@@ -18,20 +18,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MainController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
+	@Autowired
+	Test01Service test01Service;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = { "/main", "/" }, method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home(Model model) {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		model.addAttribute("serverTime", formattedDate );
-		
+		model.addAttribute("test01List", test01Service.selectTest01List());
+		System.out.println("aa: "+test01Service.selectTest01List());
 		return "/main/home";
 	}
 	
--- jc2019/src/main/webapp/WEB-INF/jsp/main/home.jsp
+++ jc2019/src/main/webapp/WEB-INF/jsp/main/home.jsp
@@ -2,17 +2,14 @@
 <head>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 	<title>Home</title>
+	<jsp:include page="/WEB-INF/jsp/common/common.jsp"/>
 </head>
 <script type="text/javascript">
 $(document).ready(function(){
-	alert('한글');
 	console.log('한글...abc...123');
 });
 </script>
 <body>
-<h1>
-	Hello world!  한글
-</h1>
-
+	${test01List}
 </body>
 </html>
